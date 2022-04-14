f9d50355613b80399e7b9ebb8642a46098f8a12a
Mustafah Hasan <79612761+leoMoste@users.noreply.github.com>
2022-04-14 23:35:20 +0200

    upload The Challenges for Programming Foundations: Fundamentals

---
+++ Favorite cities.py
@@ -0,0 +1,8 @@
+# Prints out the name of a favorite city
+def favorite_city(city_name):
+    print("One of my favorite cities is", city_name )
+
+
+favorite_city("Dubai")
+favorite_city("Weiden")
+favorite_city("Amberg")
